3f3e3ef95b0abe839da1c7ce4f03152c507c2c38
Pasindu Perakum <it21160066my.sliit.lk>
2023-12-11 13:53:58 +0530

    changes

--- src/About.js
+++ src/About.js
@@ -2,9 +2,11 @@ import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <h1>About</h1>
-    </div>
+    <main className="About">
+      <p style={{ mnarginTop: "1rem" }}>
+        This blog app is a project in the Learn React tutorial series.
+      </p>
+    </main>
   );
 };
 
--- src/SinglePageApp.js
+++ src/SinglePageApp.js
@@ -1,5 +1,6 @@
 import { Routes, Route, useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
+import { format } from "date-fns";
 import Home from "./Home";
 import NewPost from "./NewPost";
 import Missing from "./Missing";
@@ -10,35 +11,33 @@ import Nav from "./Nav";
 import Footer from "./Footer";
 
 const SinglePageApp = () => {
-  const [posts, setPosts] = useState([
-    {
-      id: 1,
-      title: "My First Post",
-      datetime: "July 01, 2021 11:17:36 AM",
-      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!",
-    },
-    {
-      id: 2,
-      title: "My 2nd Post",
-      datetime: "July 01, 2021 11:17:36 AM",
-      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!",
-    },
-    {
-      id: 3,
-      title: "My 3rd Post",
-      datetime: "July 01, 2021 11:17:36 AM",
-      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!",
-    },
-    {
-      id: 4,
-      title: "My Fourth Post",
-      datetime: "July 01, 2021 11:17:36 AM",
-      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!",
-    },
-  ]);
+  const [posts, setPosts] = useState([]);
   const [search, setSearch] = useState("");
   const [searchResults, setSearchResults] = useState([]);
   const navigate = useNavigate();
+  const [postTitle, setPostTitle] = useState("");
+  const [postBody, setPostBody] = useState("");
+
+  useEffect(() => {
+    const filteredResults = posts.filter(
+      (post) =>
+        post.body.toLowerCase().includes(search.toLowerCase()) ||
+        post.title.toLowerCase().includes(search.toLowerCase())
+    );
+    setSearchResults(filteredResults.reverse());
+  }, [posts, search]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const id = posts.length ? posts[posts.length - 1].id + 1 : 1;
+    const datetime = format(new Date(), "MMMM dd, yyyy hh:mm:ss a");
+    const newPost = { id, title: postTitle, datetime, body: postBody };
+    const allPosts = [...posts, newPost];
+    setPosts(allPosts);
+    setPostTitle("");
+    setPostBody("");
+    navigate("/");
+  };
 
   const handleDelete = (id) => {
     const postList = posts.filter((post) => post.id !== id);
@@ -51,8 +50,20 @@ const SinglePageApp = () => {
       <Header title={"React JS Blog"} />
       <Nav search={search} setSearch={setSearch} />
       <Routes>
-        <Route path="/" exact element={<Home posts={posts} />}></Route>
-        <Route path="/post" exact element={<NewPost />}></Route>
+        <Route path="/" exact element={<Home posts={searchResults} />}></Route>
+        <Route
+          path="/post"
+          exact
+          element={
+            <NewPost
+              handleSubmit={handleSubmit}
+              postTitle={postTitle}
+              setPostTitle={setPostTitle}
+              postBody={postBody}
+              setPostBody={setPostBody}
+            />
+          }
+        ></Route>
         <Route
           path="/post/:id"
           element={<PostPage posts={posts} handleDelete={handleDelete} />}
